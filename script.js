88feb2e5caaafa8cab8b3a5f37277f7495e2e346
leonardosolar <leonardosolar@yahoo.com.br>
2024-08-16 15:28:22 -0300

    feat: try catch

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
+// function saveData() {
+//     var name = document.getElementById("name").value
+//     var user = {
+//         name: name,
+//     }
+//     localStorage.setItem("userData", JSON.stringify(user))
+// }
+
+// // Exportar a função para que ela possa ser usada em testes
+// module.exports = { saveData }
+
 function saveData() {
+    // Assume que o elemento sempre existe e tem um valor
     var name = document.getElementById("name").value
     var user = {
         name: name,
     }
-    localStorage.setItem("userData", JSON.stringify(user))
+
+    // Não verifica se localStorage está disponível ou se a operação foi bem-sucedida
+    try {
+        localStorage.setItem("userData", JSON.stringify(user))
+    } catch (e) {
+        // Erro ignorado
+    }
 }
 
 // Exportar a função para que ela possa ser usada em testes
